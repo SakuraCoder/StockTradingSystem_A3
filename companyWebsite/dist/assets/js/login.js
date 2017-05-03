3046d1ab29ec0cabdfb53475ca1111003a03c7d8
iceiceiceice <335998869@qq.com>
2017-05-03 22:37:38 +0800

    修改了登录界面
    修改了登录界面，完成了前端部分

--- companyWebsite/dist/assets/js/login.js
+++ companyWebsite/dist/assets/js/login.js
@@ -2,69 +2,24 @@
  * Created by pankaicheng on 17/4/23.
  */
 
-$().ready(function() {
-    jQuery.validator.addMethod(
-        "notnumber",
-        function(value, element, param) {
-            var regex = /^\d+$/;
-            if (regex.test(value)) {return false;}
-            else {return true;}
-        },
-        $.validator.format("不能为纯数字")
-    );
-
-    $("#register_form").validate({
-        rules: {
-            username: {
-                required: true,
-                minlength: 5,
-                maxlength: 20,
-                notnumber : true
-            },
-            password: {
-                required: true,
-                minlength: 5,
-                maxlength: 20
-            },
-            rpassword: {
-                equalTo: "#register_password"
-            },
-            email: {
-                required: true,
-                email: true
-            }
-        },
-        messages: {
-            username: {
-                required: "*请输入用户名",
-                minlength: jQuery.validator.format("*用户名不能小于{0}个字符"),
-                maxlength: jQuery.validator.format("*用户名不能大于{0}个字符"),
-                notnumber: "*用户名不能是纯数字"
-            },
-            password: {
-                required: "*请输入密码",
-                minlength: jQuery.validator.format("*密码不能小于{0}个字符"),
-                maxlength: jQuery.validator.format("*密码不能大于{0}个字符")
-            },
-            rpassword: {
-                equalTo: "*两次密码不一样"
-            },
-            email: {
-                required: "*请输入邮箱",
-                email: "*请输入有效邮箱"
-            }
-        }
-    });
-});
-
 $(function() {
-    $("#register_btn").click(function() {
-        $("#register_form").css("display", "block");
-        $("#login_form").css("display", "none");
+    $('#doc-vld-msg').validator({
+        onValid: function(validity) {
+            $(validity.field).closest('.am-form-group').find('.am-u-sm-8').hide();
+        },
 
-    });
-    $("#back_btn").click(function() {
-        $("#register_form").css("display", "none");
-        $("#login_form").css("display", "block");
+        onInValid: function(validity) {
+            var $field = $(validity.field);
+            var $group = $field.closest('.am-form-group');
+            var $alert = $group.find('.am-u-sm-8');
+            // 使用自定义的提示信息 或 插件内置的提示信息
+            var msg = $field.data('validationMessage') || this.getValidationMessage(validity);
+
+            if (!$alert.length) {
+                $alert = $('<div class="am-u-sm-8 am-u-sm-offset-2 am-text-danger am-text-xs"></div>').hide().
+                appendTo($group);
+            }
+            $alert.html(msg).show();
+        }
     });
 });
