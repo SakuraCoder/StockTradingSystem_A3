7cf46d772317ce813ac82df1307369789ddca7df
iceiceiceice <335998869@qq.com>
2017-05-03 21:24:05 +0800

    注册界面
    注册界面已经完成前端部分

--- companyWebsite/dist/assets/js/register.js
+++ companyWebsite/dist/assets/js/register.js
@@ -4,18 +4,18 @@
 $(function() {
     $('#doc-vld-msg').validator({
         onValid: function(validity) {
-            $(validity.field).closest('.am-form-group').find('.am-alert').hide();
+            $(validity.field).closest('.am-form-group').find('.am-u-sm-8').hide();
         },
 
         onInValid: function(validity) {
             var $field = $(validity.field);
             var $group = $field.closest('.am-form-group');
-            var $alert = $group.find('.am-alert');
+            var $alert = $group.find('.am-u-sm-8');
             // 使用自定义的提示信息 或 插件内置的提示信息
             var msg = $field.data('validationMessage') || this.getValidationMessage(validity);
 
             if (!$alert.length) {
-                $alert = $('<div class="am-alert am-alert-danger am-u-sm-12 am-u-sm-offset-3"></div>').hide().
+                $alert = $('<div class="am-u-sm-8 am-u-sm-offset-2 am-text-danger am-text-xs"></div>').hide().
                 appendTo($group);
             }
             $alert.html(msg).show();
