9bc9e0eb8671253e306dcc35b119d3cd6f7e57b8
Sakura <632588258@qq.com>
2017-05-17 22:18:07 +0800

    new template

---
+++ AdminLTE-2.3.11/dist/img/process.py
@@ -0,0 +1,13 @@
+import cv2
+import numpy as np
+width = 1920
+height = 1080
+# img1 = cv2.imread('slide1.jpg')
+# img1 = cv2.resize(img1,(width,height))
+# cv2.imwrite('slide1.jpg',img1)
+# img1 = cv2.imread('slide2.jpg')
+# img1 = cv2.resize(img1,(width,height))
+# cv2.imwrite('slide2.jpg',img1)
+img1 = cv2.imread('slide4.jpg')
+img1 = cv2.resize(img1,(width,height))
+cv2.imwrite('slide4.jpg',img1)
